90a6bc1d55dc60b0980d84fe753fab0aff5e25d2
Ray <rayencastro00ia@gmail.com>
2025-09-06 16:19:09 -0400

    script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // Capturar el formulario
 const form = document.getElementById('formulario');
 
+// pedidos del trabajo: Implementa una función que muestre en una alerta los datos ingresados
+// en el formulario cuando se envíen. Agrega al menos un evento interactivo asociado al
+// movimiento del mouse (como onclick. onmouseover, onmouseout, etc.).
+
+// la funcion de alerta deberia ser el aviso de que el comentario es publicado.
+// el evento de click debe ser para comentar
+
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // Evita recargar la página
 
